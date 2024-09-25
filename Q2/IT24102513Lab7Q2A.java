00c838064d7afa8825dfaab75efa1cf436e740ba
IT24102513 <it24102513@my.sliit.lk>
2024-09-25 17:04:32 +0530

    IT24102513Lab7Q2A.java

---
+++ Q2/IT24102513Lab7Q2A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class IT24102513Lab7Q2A {
+public static void main(String[] args){
+
+//Define variables
+int rowCounter, columnCounter;
+
+//Entering the outside for loop
+for(rowCounter=1; rowCounter<=4; rowCounter++)
+      {
+	for(columnCounter=1;columnCounter<=5;columnCounter++)
+{  
+System.out.print(" $ ");
+}
+System.out.println();
+}
+
+	}
+ 
+}
+ 
