fb5768ce0177f50cdf820af83251cd0caaccb9da
IT24102513 <it24102513@my.sliit.lk>
2024-09-25 17:06:30 +0530

    IT24102513Lab7Q2C.java

---
+++ Q2/IT24102513Lab7Q2C.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class IT24102513Lab7Q2C {
+public static void main(String[] args) {
+
+//Defining variables
+int rowCounter,columnCounter;
+
+//Creating the nested loop with two for loops
+for (rowCounter=5; rowCounter>=1;rowCounter--) 
+	{   
+		for (columnCounter=rowCounter;columnCounter>=1;columnCounter--)
+ 		{  
+			System.out.print(rowCounter );
+		} 
+			System.out.println();
+		}
+
+	}
+ 
+}
+ 
